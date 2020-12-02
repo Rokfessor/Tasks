933e08636630a06151192d12967904b0aca9f86b
Ishbulatov Maxim <48248679+Rokfessor@users.noreply.github.com>
2020-12-02 22:45:36 +0500

    Update Main.java

--- src/Joseph/Main.java
+++ src/Joseph/Main.java
@@ -2,13 +2,16 @@ package Joseph;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println(joseph(12, 2, 2));
+        System.err.println(joseph(12, 8, 3));
     }
 
-    static int joseph (int n, int k, int s) {
+    static int joseph(int count, int step, int start) {
+        if (start > count)
+            return 0;
+
         int res = 0;
-        for (int i=s; i<=n; ++i)
-            res = (res + k + s) % (i);
-        return res + 1;
+        for (int i = 1; i <= count; ++i)
+            res = (res + step) % (i);
+        return res + start - (count * ((res + start) / count));
     }
 }
